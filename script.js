a4cbba1b3615f91ec4bf77dcfe2dcecc2b0c61c8
erisasasasasa <60240638+erisasasasasa@users.noreply.github.com>
2020-01-23 13:59:12 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".YA").dblclick(function() {
+    $(".tao").show();
+    $(".YA").hide();
+});
+$(".tao").click(function() {
+    $(".imfg").hide();
+    $(".YA").show();
+});
+$(".imfg").mouseleave(function() {
+    $("YA").hide();
+    $(".yao").hide();
+
+});
